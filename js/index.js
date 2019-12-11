601200f950cb16b375e462ee2164407b7160b58d
Suelen Duarte <suelenhenderson@gmail.com>
2019-12-11 12:28:40 -0600

    Delete index.js

--- js/index.js
+++
@@ -1,6 +0,0 @@
-// Call functions AddDay and CalculateThousandHour
-window.onload = function () {
-  document.getElementById("day").onclick = AddDay;
-  document.getElementById("calculate").onclick = CalculateThousandHour;
-
-}
